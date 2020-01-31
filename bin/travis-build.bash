56702017cbefbf38d115d047d496e4095aa00ebd
Mark Moloney <m4rkmo@gmail.com>
2020-01-31 12:21:38 +1100

    Remove deprecated 'allow-all-external' pip install option from travis build script.

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -16,8 +16,8 @@ cd ckan
 echo "CKAN branch: 2.8"
 git checkout 2.8
 python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
 cd -
 
 echo "Creating the PostgreSQL user and database..."
